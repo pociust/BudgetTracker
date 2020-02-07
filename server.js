b956cd3b12a1d01571c6813f827024470de69f00
Tom Pocius <poci14@gmail.com>
2020-02-06 18:29:19 -0600

    folders deleted

--- server.js
+++ server.js
@@ -1,34 +1,23 @@
 const express = require("express");
-const logger = require("morgan");
 const mongoose = require("mongoose");
-const htmlRoute = require("./routes/html-routes");
-const userApiRoute = require("./routes/api-routes");
-const db = require("./models");
+
+const PORT = process.env.PORT || 3000
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-app.use(logger("dev"));
-app.use(express.static(__dirname + "/public"));
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-//Store all HTML files in view folder.
+app.use(express.static("public"));
 
-app.use(htmlRoute);
-
-app.use(userApiRoute);
-
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/budget";
-const options = {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
-mongoose.connect(MONGODB_URI, options);
+  useFindAndModify: false
+});
 
-// Start the server
+// routes
+app.use(require("./routes/api.js"));
+
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
