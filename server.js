a30945869dc7055f3375ea94fb260e4b1afcc0df
Tom Pocius <poci14@gmail.com>
2020-02-06 18:33:00 -0600

    server js for herku

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+var MONGODB_URI = process.env.MONGODB_URL || 'mongodb://localhost/budget';
+const options = {
   useNewUrlParser: true,
-  useFindAndModify: false
-});
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4 // Use IPv4, skip trying IPv6
+};
+mongoose.connect(MONGODB_URI, options);
 
 // routes
-app.use(require("./routes/api.js"));
+app.use(require('./routes/api.js'));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
